5e6fda6bfdd7339e750ce111eaf1118c7644ef13
Joseph Higaki <josephhigaki@hotmail.com>
2024-12-13 15:53:46 +0000

    transform

--- src/transform/transform_json_raw_parquet.py
+++ src/transform/transform_json_raw_parquet.py
@@ -1,8 +1,13 @@
+from io import BytesIO
 import os
-#import pandas as pd
-#from datetime import datetime
-#from google.cloud import storage
+import json
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+from google.cloud import storage
 import logging
+from typing import Generator
+
 
 class SpotifyJsonToParquetTransformer:
     def __init__(self, source_bucket_name, source_path, destination_bucket_name, destination_path):
@@ -22,11 +27,45 @@ class SpotifyJsonToParquetTransformer:
         logging.basicConfig(level=logging.INFO)        
         self.logger = logging.getLogger(f"{__name__}.{self.__class__.__name__}")
 
+
+    def get_blobname_components(self, blob_name):    
+        def get_safe(list, index, default=''): 
+            return list[index] if len(list) > abs(index) else default    
+        components =blob_name.split('/')
+        username = get_safe(components, -3) 
+        date = get_safe(components, -2) 
+        filename = get_safe(components, -1)         
+        message = "Processing requires path form: /folders..../<username>/<date>/<filename>"
+        if filename == "":
+            self.logger.error(f"{message}. Filename is empty")
+        elif username == "" or date == "":
+            self.logger.warning(f"{message}. username '{username}' or date '{date}' is empty")
+        return (username, date, filename)
+
+    def iterate_files_from_gcs(self):
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(self.source_bucket_name)
+        for blob_item in bucket.list_blobs():                
+            username, date, filename = self.get_blobname_components(blob_item.name)
+            yield (username, date, filename, blob_item.download_as_string())
+        
+    def get_destination_full_path(self) -> str:
+        return f"gs://{self.destination_bucket_name}/{self.destination_path}"
+
     def transform_json_to_parquet(self):
         self.logger.info(f" {self.source_bucket_name}   {self.source_path} {self.destination_path}")
-        #for root, user_name, files in os.walk(source_bucket):
-            #for file in files:
-                #print(f"each file is {file}")
+        for username, date, filename, json_content in self.iterate_files_from_gcs():
+            if json_content:
+                df = pd.read_json(BytesIO(json_content), dtype=str) # Force everything as str, do data type discovery later
+                df['user_name'] = username
+                df['snapshot_date'] = date
+                table = pa.Table.from_pandas(df)
+                destination_full_path = self.get_destination_full_path()                
+                pq.write_to_dataset(
+                    table,
+                    root_path=destination_full_path,
+                    partition_cols=['user_name', 'snapshot_date']
+                )   
 
                     
 def main():
